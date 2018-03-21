17f2cd383ddc1d1312aa115344ea076f688bfa54
emilcecarlisa <emilcecarlisa@gmail.com>
2018-03-21 07:51:59 -0700

    Genesis of while loop

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,28 @@
 # A method to reverse each word in a sentence, in place.
 def reverse_words(my_words)
-  raise NotImplementedError
-end
+  i = 0
+  num = my_words.length
+
+  # split letters in word (create an array)
+  # while the letter in my_words is not the last letter, then
+  # flip the first letter and the last letter,
+  # reset the first letter by incrementing by one and last letter
+  # by subtracting one
+
+  while i < my_words.length
+    # temp = array[a]
+    # array[a] = array[b]
+    # array[b] = temp
+    # a += 1
+    # b -= 1
+    temp = my_words[i]
+    my_words[i] = num
+    num = temp
+
+    i += 1
+    i -= 1
+  end
+
+
+
+  end
